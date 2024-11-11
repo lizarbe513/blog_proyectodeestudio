758889ea0b943393322861ab8c783e10d7827a5c
Jkaiser <Jkaiser5@gmail.com>
2024-11-11 18:11:59 -0500

    Clase hoja de diseños bootstrap y validaciones

--- src/main/java/com/unu/blog/controllers/blogcontrolador.java
+++ src/main/java/com/unu/blog/controllers/blogcontrolador.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.unu.blog.beans.post;
@@ -13,21 +14,22 @@ import com.unu.blog.models.modelodeposts;
 
 public class blogcontrolador extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
+
 	modelodeposts modelo = new modelodeposts();
 
-    public blogcontrolador() {
-        super();
-    }
-    
-    protected void processRequest (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
-    	if (request.getParameter("tipooperacion") == null) {
-    		obtenerlista(request, response);
+	public blogcontrolador() {
+		super();
+	}
+
+	protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		if (request.getParameter("tipooperacion") == null) {
+			obtenerlista(request, response);
 			return;
 		}
-    	
+
 		String tipooperacion = request.getParameter("tipooperacion");
-		System.err.println("esta es la operacion: "+tipooperacion);
+		System.err.println("esta es la operacion: " + tipooperacion);
 		switch (tipooperacion) {
 
 		case "obtenerlista":
@@ -37,45 +39,79 @@ public class blogcontrolador extends HttpServlet {
 		case "enviarnuevopost":
 			enviarnuevopost(request, response);
 			break;
-			
+
 		case "eliminarpost":
 			eliminarpost(request, response);
 			break;
 
 		}
-    }
-    
-    private void obtenerlista(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException  {
+	}
+
+	private boolean validar(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		boolean respuesta = false;
+		List<String> listadeerrores = new ArrayList<>();
+		try {
+			if (request.getParameter("name").equals("")) {
+				respuesta = true;
+				listadeerrores.add("Ingrese nombre del post");
+			}
+			if (request.getParameter("postcont").equals("")) {
+				respuesta = true;
+				listadeerrores.add("Ingrese contenido al post");
+			}
+			
+			request.setAttribute("respuesta", respuesta);
+			request.setAttribute("listadeerrores", listadeerrores);
+		} catch (Exception e) {
+			e.getStackTrace();
+		}
+
+		return respuesta;
+	}
+
+	private void obtenerlista(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		request.setAttribute("lista", modelo.obtenerlistadeposts());
 		request.getRequestDispatcher("/posts/listadeposts.jsp").forward(request, response);
 	}
-    
-    public void enviarnuevopost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String postname = request.getParameter("postname");
-        String postcont = request.getParameter("postcont");
-        post nuevoPost = new post(postname, postcont);
-    	if (nuevoPost != null) {
-            modelo.nuevopost(nuevoPost);
-            obtenerlista(request, response);
-        }
-	}
-    
-    public void eliminarpost(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException{
-    	int ideliminar = Integer.parseInt(request.getParameter("idposteliminar"));
-    	if(ideliminar > 0) {
-    		modelo.eliminarpost(ideliminar);
-    		obtenerlista(request, response);
-    	}
-    }
-       
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public void enviarnuevopost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		try {
+			if (!validar(request, response)) {
+				String postname = request.getParameter("postname");
+				String postcont = request.getParameter("postcont");
+				post nuevoPost = new post(postname, postcont);
+				if (nuevoPost != null) {
+					modelo.nuevopost(nuevoPost);
+					obtenerlista(request, response);
+				}
+			}
+			response.sendRedirect(request.getContextPath()+"/blogcontrolador?tipooperacion=obtenerlista");
+		} catch (Exception e) {
+			request.getRequestDispatcher("/posts/nuevopost.jsp").forward(request, response);
+		}
+
+	}
+
+	public void eliminarpost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		int ideliminar = Integer.parseInt(request.getParameter("idposteliminar"));
+		if (ideliminar > 0) {
+			modelo.eliminarpost(ideliminar);
+			obtenerlista(request, response);
+		}
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		processRequest(request, response);
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		processRequest(request, response);
 	}
-	
 
 }
--- src/main/webapp/posts/nuevopost.jsp
+++ src/main/webapp/posts/nuevopost.jsp
@@ -2,20 +2,50 @@
     pageEncoding="UTF-8"%>
 <%@ page import="com.unu.blog.controllers.blogcontrolador"%>
 <%@ page import="com.unu.blog.beans.post"%>
+<%@ page import="java.util.List"%>
+
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
 <title>Insert title here</title>
+
+<script>
+	function validarFormulario (){
+		 const nombre = document.getElementById('postname').value.trim();
+		 const postcont = document.getElementById('postcont').value.trim();
+		 
+		 if(nombre === ''){
+			 alert('Ingrese nombre del post');
+			 document.getElementById('postname').focus();
+			 return false;
+		 }
+		 
+		 return true;
+	}
+</script>
+
 </head>
 <body>
 <%
 	String url="http://localhost:8080/blog/"; 
+	if(request.getParameter("respuesta")!=null){
+		boolean respuesta=(boolean) request.getAttribute("respuesta");
+		if(respuesta){
+			List<String> listadeerrores = (List<String>) request.getAttribute("listadeerrores");
+			for(String errori: listadeerrores){
+				%>
+				<span><%=errori %></span>
+				<br>
+				<%
+			}
+		}
+	}
 %>
 
 <h3>Agregar nuevo post:</h3>
 
-<form action="<%=url%>blogcontrolador" method="post">
+<form action="<%=url%>blogcontrolador" method="post" onsubmit="return validarFormulario()">
 	<input type="hidden" name="tipooperacion" value="enviarnuevopost">
 	
     <label for="name">name:</label>
